fbd5b7edd062a732eadb6d0ce9e2d0f8fa9e0eeb
Lee.m.yin <lee@Leemyins-MacBook-Pro.local>
2026-01-13 07:45:50 +0800

    Tue Jan 13 07:45:50 CST 2026

--- js/download.js
+++ js/download.js
@@ -47,7 +47,7 @@ class DownloadManager {
 
             if (!downloadInfos) return;
 
-            console.log('DownloadInfos for platform', platformKey, downloadInfos);
+            // console.log('DownloadInfos for platform', platformKey, downloadInfos);
             downloadInfos.forEach(downloadInfo => {
                 const platformCard = this.createPlatformCard(platform, downloadInfo, this.currentVersion);
                 platformsGrid.appendChild(platformCard);
